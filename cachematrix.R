2d6aa95687642212028a144f6f4fe138963b080e
khispa <khispa@gmail.com>
2015-09-19 10:43:06 +0200

    Functions working

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,35 @@
-## Put comments here that give an overall description of what your
-## functions do
+## Those functions combined calculate the inverse of a given matrix that is invertible and store it in Cache memory.
+## If the same matrix inversion is required again, the cache values are used, intead of calculate them again.
 
-## Write a short comment describing this function
+## makeCacheMatrix creates an S4 object that is able to store in cache the inverse of a given matrix
 
 makeCacheMatrix <- function(x = matrix()) {
-
+  i <- NULL
+  set <- function(y) {
+    x <<- y
+    i <<- NULL
+  }
+  get <- function() x
+  setinv <- function(solve) i <<- solve(x)
+  getinv <- function() i
+  list(set = set, get = get,
+           setinv = setinv,
+           getinv = getinv)
 }
 
 
-## Write a short comment describing this function
+## cacheSolve takes the formal class output from previous function and calculates the inverse or 
+## returns the cache values if it has been calculated before. 
+## Remember: x must be S4 object from previous function.
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+  i <- x$getinv()
+  if(!is.null(i)) {
+    message("getting cached data")
+    return(i)
+  }
+  data <- x$get()
+  i <- solve(data, ...)
+  x$setinv(i)
+  i
 }
